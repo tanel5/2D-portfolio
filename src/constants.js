93ba26cea4c688ac8a77f3d213f3040f6a57a6e0
tanel5 <57736087+tanel5@users.noreply.github.com>
2024-08-15 19:42:17 +0200

    first commit

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `Ceci est mon PC. Je travaille dans différent langages sur des projets backend et frontend. 
+      je code surtout en python et php avec les framework django et symfony, mais peu importe le language j'aime bien programmer.
+      Voici mon <a href="https://github.com/tanel5" target="_blank">Github</a>!`,
+    "sofa-table": `Voici mon canapé ou je me detends, regarde des vidéos ou je joue à des jeux videos`,
+    tv: `Voici ma télévision. je regarde beaucoup de tutoriel dessus That's my TV.`,
+    bed: `Voici ou je dors. De grandes idées viennent quand je m'allonge sur le lit.`,
+    resume: `Tvoici mon bureau et mon CV. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Clickez si ca vous interesse?</a>`,
+    projects: `Information à propos de ce portfolio: il est fait avec la librairie Kaboom.js qui est une librairie pour faire des jeux en javascript.
+    le texte est rendu avec HTML/CSS.`,
+    library: `Il y a beaucoup de livre de programmation sur cet étagère. `,
+    exit: `Si vous souhaitez quitter ce portfolio, vous devez juste fermez l'onglet.`,
+  };
